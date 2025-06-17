b6ad1a9fa75a18fcc1c6e7bf6ac5a61438e1fbd7
thongdoc <thongdoc0508@gmail.com>
2025-06-17 10:44:33 +0700

    Refactor market cap retrieval and caching logic in DataFeedService to handle undefined values and improve error handling.

--- open/src/data-feed/data-feed.service.ts
+++ open/src/data-feed/data-feed.service.ts
@@ -41,25 +41,25 @@ export class DataFeedService {
               'x-cg-pro-api-key': COINGECKO_API_KEY
             }
           })
-          marketCap = res.data[symbol].usd_market_cap
-          // save to redis
-          const key = `market_cap:${m.address}`
-
-          if (!marketCap) {
-            const redisMarketCap = await this.redis.get(key)
-            if (redisMarketCap) {
-              marketCap = Number(redisMarketCap)
-            }
-          } else {
-            await this.redis.set(key, marketCap, 60 * 60 * 24)
-          }
+          marketCap = res.data[symbol]?.usd_market_cap || 0
         } catch (error) {}
+        // save to redis
+        const key = `market_cap:${m.address}`
+
+        if (!marketCap) {
+          const redisMarketCap = await this.redis.get(key)
+          if (redisMarketCap) {
+            marketCap = Number(redisMarketCap)
+          }
+        } else {
+          await this.redis.set(key, marketCap, 60 * 60 * 24)
+        }
         return {
           address: m.address,
           name: m.name,
           decimals: m.adapter.decimals,
           lastedPrice: m.Aggregate[0].value.toString(),
-          marketCap: marketCap?.data[symbol]?.usd_market_cap || 0,
+          marketCap: marketCap || 0,
           chainId: m.Chain.chainId,
           chainName: m.Chain.name,
           iconUrl: m.Chain.iconUrl,
