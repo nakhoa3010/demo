ffa4405f190fb20916e9ce9afaf9eaab1a1a0c48
thongdoc <thongdoc0508@gmail.com>
2025-06-17 22:39:15 +0700

    Update root method response in AppService to reflect new API name

--- open/src/app.service.ts
+++ open/src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common'
 @Injectable()
 export class AppService {
   root() {
-    return 'ADCS API'
+    return 'X Oracle Open API'
   }
   health(): string {
     return 'OK'
